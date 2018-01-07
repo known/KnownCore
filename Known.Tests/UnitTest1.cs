85da0959961cef71f6c7488cdd2f096514629e67
Known Chen <knownchen@163.com>
2018-01-07 22:48:19 +0800

    add frame

---
+++ Known.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Known.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
