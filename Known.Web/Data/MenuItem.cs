7fcfc4d4009f5f6178cfb9ae9ffb1a425468d32c
Known <knownchen@163.com>
2020-06-26 23:08:29 +0800

    Update

---
+++ Known.Web/Data/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Known.Web.Data
+{
+    public class MenuItem
+    {
+        private readonly List<MenuItem> _items = new List<MenuItem>();
+        private MenuItem Parent { get; set; }
+
+        public IEnumerable<MenuItem> Items => _items;
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+
+        public void AddItem(MenuItem item)
+        {
+            item.Parent = this;
+            _items.Add(item);
+        }
+    }
+}
--- Known.Web/Startup.cs
+++ Known.Web/Startup.cs
@@ -1,8 +1,6 @@
+using Known.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,15 +17,12 @@ namespace Known.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,31 +35,16 @@ namespace Known.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
